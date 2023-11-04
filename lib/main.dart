e226cec996aefe1837216525323886e3acede603
bajnsk <baekjunseok63@gmail.com>
2023-11-05 04:11:55 +0900

    메인 Provider 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:capstone/login_page/providers/auth_provider.dart';
+import 'package:capstone/login_page/repositories/auth_repository.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:capstone/login_page/LoginPage.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'firebase_options.dart';
+import 'package:provider/provider.dart';
 
-
+import 'login_page/providers/auth_state.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,14 +23,27 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Asher',
-      debugShowCheckedModeBanner: false, // 디버그 표시 제거
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        Provider<AuthRepository>(
+          create: (context) => AuthRepository(
+            firebaseAuth: FirebaseAuth.instance,
+            firebaseFirestore: FirebaseFirestore.instance,
+            firebaseStorage: FirebaseStorage.instance,
+          ),
+        ),
+        StateNotifierProvider<AuthProvider, AuthState>(
+          create: (context) => AuthProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Asher',
+        debugShowCheckedModeBanner: false, // 디버그 표시 제거
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: LoginPage(),
       ),
-      home: LoginPage(),
     );
   }
 }
-
