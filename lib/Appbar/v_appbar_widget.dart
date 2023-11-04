c5677ff80271488da951fd0dac438bc16621753b
nam-kiyun <fordnam1@gmail.com>
2023-11-04 14:39:58 +0900

    appbar 위젯 수정

--- lib/Appbar/v_appbar_widget.dart
+++ lib/Appbar/v_appbar_widget.dart
@@ -1,76 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Appbar extends StatefulWidget {
-  const Appbar({super.key});
-
+class AsherAppbar extends StatelessWidget implements PreferredSizeWidget {
   @override
-  State<Appbar> createState() => _AppbarState();
-}
-
-class _AppbarState extends State<Appbar> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        title: Text(
-          'Asher',
-          style: GoogleFonts.bebasNeue(
-            fontSize: 30,
-            color: Colors.grey[700],
-          ),
-        ),
-        backgroundColor: Colors.grey[300],
-        centerTitle: true,
-        leading: IconButton(
-          icon: Icon(Icons.menu, color: Colors.black, size: 30),
-          onPressed: () {
-            _scaffoldKey.currentState!.openDrawer();
-          },
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 40),
-            child: IconButton(
-              onPressed: () {
-                // 버튼을 눌렀을 때 수행할 동작 추가할 것
-              },
-              icon: Icon(Icons.notifications_none, color: Colors.black, size: 30),
+    return AppBar(
+      title: Padding(
+        padding: const EdgeInsets.only(left: 20), // 왼쪽 여백 추가
+        child: Row(
+          children: [
+            Text(
+              'Asher',
+              style: GoogleFonts.bebasNeue(
+                fontSize: 30,
+                color: Colors.grey[700],
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: 20),
-            child: IconButton(
-              onPressed: () {
-                // 버튼을 눌렀을 때 수행할 동작 추가할 것
-              },
-              icon: Icon(Icons.mail_outline, color: Colors.black, size: 30),
-            ),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text('메뉴 항목 1'),
-              onTap: () {
-                // 메뉴 항목 1이 선택되었을 때 수행할 작업
-              },
-            ),
-            ListTile(
-              title: Text('메뉴 항목 2'),
-              onTap: () {
-                // 메뉴 항목 2가 선택되었을 때 수행할 작업
-              },
-            ),
-            // 추가적인 메뉴 항목들...
           ],
         ),
       ),
+      backgroundColor: Colors.grey[300],
+      actions: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(right: 5),
+          child: IconButton(
+            onPressed: () {
+              // 버튼을 눌렀을 때 수행할 동작 추가할 것
+            },
+            icon: Icon(Icons.notifications_none, color: Colors.black, size: 30),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(right: 5),
+          child: IconButton(
+            onPressed: () {
+              // 버튼을 눌렀을 때 수행할 동작 추가할 것
+            },
+            icon: Icon(Icons.mail_outline, color: Colors.black, size: 30),
+          ),
+        ),
+      ],
     );
   }
 }
+
+
