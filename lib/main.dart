1af4cfe79175b7266ad3ce6f3507fbb78fe71953
nam-kiyun <fordnam1@gmail.com>
2023-11-07 13:59:53 +0900

    경로 수정 및 main.dart feedprovider 설정? 연결 +flutter pub get 할것

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:capstone/login_page/providers/auth_provider.dart';
+import 'package:capstone/login_page/providers/auth/auth_provider.dart';
+import 'package:capstone/login_page/providers/feed/feed_provider.dart';
+import 'package:capstone/login_page/providers/feed/feed_state.dart';
 import 'package:capstone/login_page/repositories/auth_repository.dart';
+import 'package:capstone/login_page/repositories/feed_repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -9,8 +12,8 @@ import 'package:capstone/login_page/LoginPage.dart';
 import 'package:flutter_state_notifier/flutter_state_notifier.dart';
 import 'firebase_options.dart';
 import 'package:provider/provider.dart';
+import 'login_page/providers/auth/auth_state.dart';
 
-import 'login_page/providers/auth_state.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,8 +35,17 @@ class MyApp extends StatelessWidget {
             firebaseStorage: FirebaseStorage.instance,
           ),
         ),
+        Provider<FeedRepository>(
+          create: (context) => FeedRepository(
+            firebaseFirestore: FirebaseFirestore.instance,
+            firebaseStorage: FirebaseStorage.instance,
+          ),
+        ),
         StateNotifierProvider<AuthProvider, AuthState>(
           create: (context) => AuthProvider(),
+        ),
+        StateNotifierProvider<FeedProvider, FeedState>(
+            create: (context) => FeedProvider(),
         ),
       ],
       child: MaterialApp(
