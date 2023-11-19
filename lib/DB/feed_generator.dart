db3ce8613f6e375f71de2f1d02517726f244cfdd
bajnsk <baekjunseok63@gmail.com>
2023-11-19 22:07:37 +0900

    피드 작성 페이지 친구추가 기능 숨김

--- lib/DB/feed_generator.dart
+++ lib/DB/feed_generator.dart
@@ -17,7 +17,7 @@ class FeedGenerator extends StatefulWidget {
 
 class _FeedGeneratorState extends State<FeedGenerator> {
   final TextEditingController _contentController = TextEditingController();
-  final TextEditingController _friendNameController = TextEditingController();
+  // final TextEditingController _friendNameController = TextEditingController();
   final TextEditingController _tagController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
   Uint8List? _imageBytes;
@@ -123,38 +123,38 @@ class _FeedGeneratorState extends State<FeedGenerator> {
   }
 
   // 친구를 추가하는 함수
-  Future<void> addFriendByName(String friendName) async {
-    try {
-      User? user = _auth.currentUser;
-
-      if (user != null) {
-        // 이름으로 해당 사용자를 찾음
-        QuerySnapshot<Map<String, dynamic>> querySnapshot = await _firestore
-            .collection('users')
-            .where('name', isEqualTo: friendName)
-            .get();
-
-        if (querySnapshot.docs.isNotEmpty) {
-          // 찾은 사용자의 UID를 가져와서 친구 목록에 추가
-          String friendUid = querySnapshot.docs.first.id;
-          await _firestore.collection('users').doc(user.uid).update({
-            'friends': FieldValue.arrayUnion([friendUid]),
-          });
-
-          // 친구에게도 나를 친구로 등록
-          await _firestore.collection('users').doc(friendUid).update({
-            'friends': FieldValue.arrayUnion([user.uid]),
-          });
-
-          logger.d('Friend added successfully.');
-        } else {
-          logger.d('User with name $friendName not found.');
-        }
-      }
-    } catch (e) {
-      logger.d('Error adding friend: $e');
-    }
-  }
+  // Future<void> addFriendByName(String friendName) async {
+  //   try {
+  //     User? user = _auth.currentUser;
+  //
+  //     if (user != null) {
+  //       // 이름으로 해당 사용자를 찾음
+  //       QuerySnapshot<Map<String, dynamic>> querySnapshot = await _firestore
+  //           .collection('users')
+  //           .where('name', isEqualTo: friendName)
+  //           .get();
+  //
+  //       if (querySnapshot.docs.isNotEmpty) {
+  //         // 찾은 사용자의 UID를 가져와서 친구 목록에 추가
+  //         String friendUid = querySnapshot.docs.first.id;
+  //         await _firestore.collection('users').doc(user.uid).update({
+  //           'friends': FieldValue.arrayUnion([friendUid]),
+  //         });
+  //
+  //         // 친구에게도 나를 친구로 등록
+  //         await _firestore.collection('users').doc(friendUid).update({
+  //           'friends': FieldValue.arrayUnion([user.uid]),
+  //         });
+  //
+  //         logger.d('Friend added successfully.');
+  //       } else {
+  //         logger.d('User with name $friendName not found.');
+  //       }
+  //     }
+  //   } catch (e) {
+  //     logger.d('Error adding friend: $e');
+  //   }
+  // }
 
   // _pickImage 함수를 수정하여 XFile을 반환하도록 수정
   Future<void> _pickImage() async {
@@ -289,25 +289,25 @@ class _FeedGeneratorState extends State<FeedGenerator> {
                 child: Text('Add Feed to Firestore'),
               ),
               SizedBox(height: 16.0),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                      controller: _friendNameController,
-                      decoration: InputDecoration(labelText: 'Friend Name'),
-                    ),
-                  ),
-                  SizedBox(width: 8.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      // // 텍스트 필드에서 얻은 이름으로 친구 추가
-                      addFriendByName(_friendNameController.text);
-                    },
-                    child: Text('Add Friend'),
-                  ),
-                  SizedBox(width: 8.0),
-                ],
-              ),
+              // Row(
+              //   children: [
+              //     Expanded(
+              //       child: TextField(
+              //         controller: _friendNameController,
+              //         decoration: InputDecoration(labelText: 'Friend Name'),
+              //       ),
+              //     ),
+              //     SizedBox(width: 8.0),
+              //     ElevatedButton(
+              //       onPressed: () {
+              //         // // 텍스트 필드에서 얻은 이름으로 친구 추가
+              //         addFriendByName(_friendNameController.text);
+              //       },
+              //       child: Text('Add Friend'),
+              //     ),
+              //     SizedBox(width: 8.0),
+              //   ],
+              // ),
             ],
           ),
         ),
