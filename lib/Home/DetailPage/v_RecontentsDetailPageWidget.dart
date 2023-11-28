543d0914ec625139b4602117f7f4190ab5d66d8e
김재형 <Hiruca3220@gmail.com>
2023-11-29 01:36:32 +0900

    리컨텐츠 피드 디테일뷰 수정 50퍼센트 진행 / 내일 마무리

--- lib/Home/DetailPage/v_RecontentsDetailPageWidget.dart
+++ lib/Home/DetailPage/v_RecontentsDetailPageWidget.dart
@@ -1,15 +1,90 @@
+import 'package:capstone/DataVO/model.dart';
+import 'package:capstone/Home/FeedPage/c_FeedPageController.dart';
 import 'package:flutter/material.dart';
 
 class RecontentsDetailPageWidget extends StatefulWidget {
-  const RecontentsDetailPageWidget({super.key});
+  const RecontentsDetailPageWidget(
+      {super.key, required this.feedData, required this.isFavorite});
 
+  final FeedDataVO feedData;
+  final bool isFavorite;
   @override
-  State<RecontentsDetailPageWidget> createState() => _RecontentsDetailPageWidgetState();
+  State<RecontentsDetailPageWidget> createState() =>
+      _RecontentsDetailPageWidgetState();
 }
 
-class _RecontentsDetailPageWidgetState extends State<RecontentsDetailPageWidget> {
+class _RecontentsDetailPageWidgetState
+    extends State<RecontentsDetailPageWidget> {
+  late List<String> recontentsFeedIds = ['null', 'null'];
+  late FeedDataVO originalFeed;
+  late FeedDataVO recontentsFeed;
+  int currentPageIndex = 0;
+
   @override
+  void initState() {
+    super.initState();
+
+    setFeedIds();
+    setFeedData();
+  }
+
+  void setFeedIds() {
+    recontentsFeedIds[0] = widget.feedData.feedId.toString();
+    for (FeedDataVO fd in DataVO.feedData) {
+      if (fd.feedId == widget.feedData.reContentId) {
+        recontentsFeedIds[1] = fd.feedId.toString();
+      }
+    }
+    print('리컨텐츠 아이디들임');
+    print(recontentsFeedIds[0]);
+    print(recontentsFeedIds[1]);
+  }
+
+  void setFeedData() {
+    // originalFeed 설정
+    originalFeed = DataVO.feedData.firstWhere(
+      (feed) => feed.feedId.toString() == recontentsFeedIds[0],
+      orElse: () => FeedDataVO(
+        feedId: 'null',
+        userId: 'null',
+        reContentId: 'null',
+        context_text: 'null',
+        image: [],
+        makeTime: 'null',
+        tag: [],
+        userName: 'null',
+        userProfile: 'null',
+      ),
+    );
+
+    // recontentsFeed 설정
+    recontentsFeed = DataVO.feedData.firstWhere(
+      (feed) => feed.feedId.toString() == recontentsFeedIds[1],
+      orElse: () => FeedDataVO(
+        feedId: 'null',
+        userId: 'null',
+        reContentId: 'null',
+        context_text: 'null',
+        image: [],
+        makeTime: 'null',
+        tag: [],
+        userName: 'null',
+        userProfile: 'null',
+      ),
+    );
+
+    // originalFeed과 recontentsFeed를 출력
+    print('originalFeed:');
+    print(originalFeed);
+    print('recontentsFeed:');
+    print(recontentsFeed);
+  }
+
   Widget build(BuildContext context) {
+    String recontentsFeedMakeTime =
+        FeedTypeController.internal().makeTimeTodate(recontentsFeed);
+    String originalFeedMakeTime =
+        FeedTypeController.internal().makeTimeTodate(originalFeed);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.grey[300],
@@ -24,69 +99,112 @@ class _RecontentsDetailPageWidgetState extends State<RecontentsDetailPageWidget>
       body: SingleChildScrollView(
         child: Column(
           children: [
-            //리컨텐츠된 피드 컨테이너
+            // 리컨텐츠된 피드 컨테이너
             Container(
               width: MediaQuery.of(context).size.width,
               height: null,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  //타임 스탬프 컨테이너
+                  // 타임 스탬프 컨테이너
                   Container(
                     margin: EdgeInsets.only(left: 30, right: 30, top: 10),
                     child: Text(
-                      'date',
+                      recontentsFeedMakeTime,
                       style: TextStyle(fontSize: 16, color: Colors.grey),
                     ),
                   ),
-                  //사진 컨테이너
+                  // 사진 컨테이너
                   Container(
-                    margin: EdgeInsets.only(top: 10,left: 25),
-                    width: MediaQuery.of(context).size.width - 50,
-                    height: 200,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.grey
+                    height: 300,
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(
+                      child: Stack(alignment: Alignment.topRight, children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 50, right: 50, top: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: PageView.builder(
+                                itemCount: originalFeed.image.length,
+                                onPageChanged: (value) {
+                                  setState(() {
+                                    currentPageIndex = value;
+                                  });
+                                },
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Image.network(
+                                    originalFeed.image.isNotEmpty &&
+                                            originalFeed.image.length > 0
+                                        ? originalFeed.image[index]
+                                        : 'https://firebasestorage.googleapis.com/v0/b/capstone2-1ad1d.appspot.com/o/smile%20pepe.png?alt=media&token=2240d1b9-b8cd-472a-81b8-d4f3be82335b',
+                                    fit: BoxFit.cover,
+                                    width:
+                                        MediaQuery.of(context).size.width - 100,
+                                    height: 280,
+                                  );
+                                }),
+                          ),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            margin: const EdgeInsets.only(top: 20, right: 60),
+                            decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.8),
+                                borderRadius: BorderRadius.circular(500)),
+                            child: Text(
+                              '${currentPageIndex + 1}/${originalFeed.image.length}',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                      ]),
                     ),
                   ),
-                  SizedBox(height: 15,),
-                  //본문 컨테이너
+                  SizedBox(
+                    height: 15,
+                  ),
+                  // 본문 컨테이너
                   Container(
                     width: MediaQuery.of(context).size.width - 50,
                     margin: EdgeInsets.only(left: 25),
-                    child: Text(
-                        '본문텍스트'
-                    ),
+                    child: Text(originalFeed.context_text), // 변경된 부분
                   ),
-                  SizedBox(height: 15,),
-                  //태그 컨테이너
+                  SizedBox(
+                    height: 15,
+                  ),
+                  // 태그 컨테이너
                   Container(
                     width: MediaQuery.of(context).size.width - 50,
                     margin: EdgeInsets.only(left: 25),
-                    child: Text(
-                        '태그'
-                    ),
+                    child: Text(originalFeed.tag
+                        .join(', ')
+                        .substring(1, originalFeed.tag.join(', ').length - 1)),
+                    // 변경된 부분
                   ),
                 ],
               ),
             ),
 
-            //리컨텐츠 아이콘
+            // 리컨텐츠 아이콘
             Icon(
               Icons.attach_file,
               color: Colors.grey.withOpacity(0.7),
               size: 40,
             ),
-            SizedBox(height: 10,),
+            SizedBox(
+              height: 10,
+            ),
 
-            //리컨텐츠해 새로 작성된 피드 컨테이너
+            // 리컨텐츠해 새로 작성된 피드 컨테이너
             Container(
               width: MediaQuery.of(context).size.width,
               height: null,
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  //프로필 및 유저이름 컨테이너
+                  // 프로필 및 유저이름 컨테이너
                   Container(
                     height: 50,
                     width: MediaQuery.of(context).size.width,
@@ -112,8 +230,8 @@ class _RecontentsDetailPageWidgetState extends State<RecontentsDetailPageWidget>
                           //),
                         ),
                         SizedBox(width: 10),
-                        Text('feedData.userName'),
-                        Spacer(), // 여기에 Spacer 추가
+                        Text(recontentsFeed.userName), // 변경된 부분
+                        Spacer(),
                         InkWell(
                           onTap: () {
                             // Add the action for the more_vert icon
@@ -124,80 +242,93 @@ class _RecontentsDetailPageWidgetState extends State<RecontentsDetailPageWidget>
                       ],
                     ),
                   ),
-                  //새로 생성된 타임스탬프 컨테이너
+                  // 새로 생성된 타임스탬프 컨테이너
                   Container(
-                    margin: EdgeInsets.only(left: 30, right: 30,top: 5),
+                    margin: EdgeInsets.only(left: 30, right: 30, top: 5),
                     child: Text(
-                      'date',
+                      originalFeedMakeTime, // 변경된 부분
                       style: TextStyle(fontSize: 16, color: Colors.grey),
                     ),
                   ),
-                  //새로 생성된 사진 컨테이너
+                  // 새로 생성된 사진 컨테이너
                   Container(
-                    margin: EdgeInsets.only(top: 10,left: 25),
-                    width: MediaQuery.of(context).size.width - 50,
-                    height: 200,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.grey
+                    height: 300,
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(
+                      child: Stack(alignment: Alignment.topRight, children: [
+                        Container(
+                          margin: EdgeInsets.only(left: 50, right: 50, top: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: PageView.builder(
+                                itemCount: recontentsFeed.image.length,
+                                onPageChanged: (value) {
+                                  setState(() {
+                                    currentPageIndex = value;
+                                  });
+                                },
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Image.network(
+                                    recontentsFeed.image.isNotEmpty &&
+                                            recontentsFeed.image.length > 0
+                                        ? recontentsFeed.image[index]
+                                        : 'https://firebasestorage.googleapis.com/v0/b/capstone2-1ad1d.appspot.com/o/smile%20pepe.png?alt=media&token=2240d1b9-b8cd-472a-81b8-d4f3be82335b',
+                                    fit: BoxFit.cover,
+                                    width:
+                                        MediaQuery.of(context).size.width - 100,
+                                    height: 280,
+                                  );
+                                }),
+                          ),
+                        ),
+                        Container(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 5, horizontal: 10),
+                            margin: const EdgeInsets.only(top: 20, right: 60),
+                            decoration: BoxDecoration(
+                                color: Colors.black.withOpacity(0.8),
+                                borderRadius: BorderRadius.circular(500)),
+                            child: Text(
+                              '${currentPageIndex + 1}/${recontentsFeed.image.length}',
+                              style: TextStyle(color: Colors.white),
+                            )),
+                      ]),
                     ),
                   ),
-                  SizedBox(height: 15,),
-                  //새로 생성된 본문 컨테이너
+                  SizedBox(
+                    height: 15,
+                  ),
+                  // 새로 생성된 본문 컨테이너
                   Container(
                     width: MediaQuery.of(context).size.width - 50,
                     margin: EdgeInsets.only(left: 25),
-                    child: Text(
-                        '본문텍스트'
-                    ),
+                    child: Text(recontentsFeed.context_text), // 변경된 부분
                   ),
-                  SizedBox(height: 15,),
-                  //새로 생성된 태그 컨테이너
+                  SizedBox(
+                    height: 15,
+                  ),
+                  // 새로 생성된 태그 컨테이너
                   Container(
                     width: MediaQuery.of(context).size.width - 50,
                     margin: EdgeInsets.only(left: 25),
-                    child: Text(
-                        '태그'
-                    ),
+                    child: Text(recontentsFeed.tag.join(', ').substring(
+                        1, recontentsFeed.tag.join(', ').length - 1)),
+                    // 변경된 부분
                   ),
-                  SizedBox(height: 15,),
-                  //하트 및 각종 아이콘 컨테이너
+                  SizedBox(
+                    height: 15,
+                  ),
+                  // 하트 및 각종 아이콘 컨테이너
                   Container(
-                    margin: EdgeInsets.only(left: 25,right: 25),
+                    margin: EdgeInsets.only(left: 25, right: 25),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Row(
                           children: [
-                            /*
-                            LikeButton(
-                              isLiked: isFavorite,
-                              onTap: (bool isLiked) {
-                                // 하트 버튼을 눌렀을 때
-                                setState(() {
-                                  isFavorite = !isFavorite;
-                                  // DataVO.myUserData.likeFeeds 업데이트
-                                  if (isFavorite) {
-                                    DataVO.myUserData.likeFeed.add(feedData.feedId);
-                                    FeedTypeController.instance
-                                        .likeFeedToFirebase(feedData);
-                                  } else {
-                                    DataVO.myUserData.likeFeed.remove(feedData.feedId);
-                                    FeedTypeController.instance
-                                        .unlikeFeedFromFirebase(feedData);
-                                  }
-                                });
-                                return Future.value(!isLiked);
-                              },
-                              likeBuilder: (bool isLiked) {
-                                return Icon(
-                                  isLiked ? Icons.favorite : Icons.favorite_border,
-                                  color: isLiked ? Colors.red : Colors.grey,
-                                );
-                              },
-                            ),
-
-                             */
                             InkWell(
                               onTap: () {
                                 // Add the action for the message icon
@@ -209,7 +340,6 @@ class _RecontentsDetailPageWidgetState extends State<RecontentsDetailPageWidget>
                             ),
                           ],
                         ),
-                        // Spacer 추가
                         Spacer(),
                         InkWell(
                           onTap: () {
@@ -224,7 +354,9 @@ class _RecontentsDetailPageWidgetState extends State<RecontentsDetailPageWidget>
                       ],
                     ),
                   ),
-                  SizedBox(height: 15,),
+                  SizedBox(
+                    height: 15,
+                  ),
                 ],
               ),
             )
--- lib/Home/FeedPage/v_FeedCardWidget.dart
+++ lib/Home/FeedPage/v_FeedCardWidget.dart
@@ -1,3 +1,4 @@
+import 'package:capstone/Home/DetailPage/v_RecontentsDetailPageWidget.dart';
 import 'package:capstone/Home/FeedPage//c_FeedPageController.dart';
 import 'package:capstone/Home/DetailPage/v_DetailPageWidget.dart';
 import 'package:flutter/material.dart';
@@ -84,19 +85,17 @@ class FeedCardState extends State<FeedCardWidget> {
             ),
           ),
           // 사진 컨테이너
-          Stack(
-            children: [
-              feedData.reContentId != null
-              ? Positioned(
-                top: 20,
-                left: 10,
-                child: Icon(
-                  Icons.attach_file
-                ),
-              )
-              : Container(),
-              GestureDetector(
-                onTap: () async {
+          Stack(children: [
+            feedData.reContentId != null
+                ? Positioned(
+                    top: 20,
+                    left: 10,
+                    child: Icon(Icons.attach_file),
+                  )
+                : Container(),
+            GestureDetector(
+              onTap: () async {
+                if (widget.feedData.reContentId == null) {
                   var returnedValue = await Navigator.push(
                     context,
                     MaterialPageRoute(
@@ -111,72 +110,102 @@ class FeedCardState extends State<FeedCardWidget> {
                       isFavorite = returnedValue;
                     });
                   }
-                },
-                child: Container(
-                  height: 250,
-                  width: MediaQuery.of(context).size.width,
-                  child: Center(
-                    child: Stack(alignment: Alignment.topRight, children: [
-                      Container(
-                        margin: EdgeInsets.only(left: 50, right: 50, top: 10),
-                        width: MediaQuery.of(context).size.width - 100,
-                        height: 230,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                        ),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: PageView.builder(
-                              itemCount: feedData.image.length,
-                              onPageChanged: (value) {
-                                setState(() {
-                                  currentPageIndex = value;
-                                });
-                              },
-                              itemBuilder: (BuildContext context, int index) {
-                                return Image.network(
-                                  feedData.image.isNotEmpty &&
-                                      feedData.image.length > 0
-                                      ? feedData.image[index]
-                                      : 'https://firebasestorage.googleapis.com/v0/b/capstone2-1ad1d.appspot.com/o/smile%20pepe.png?alt=media&token=2240d1b9-b8cd-472a-81b8-d4f3be82335b',
-                                  fit: BoxFit.cover,
-                                );
-                              }),
-                        ),
+                } else {
+                  var returnedValue = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => RecontentsDetailPageWidget(
+                              feedData: feedData,
+                              isFavorite: isFavorite,
+                            )),
+                  );
+                  if (returnedValue != null && returnedValue is bool) {
+                    setState(() {
+                      isFavorite = returnedValue;
+                    });
+                  }
+                }
+              },
+              child: Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width,
+                child: Center(
+                  child: Stack(alignment: Alignment.topRight, children: [
+                    Container(
+                      margin: EdgeInsets.only(left: 50, right: 50, top: 10),
+                      width: MediaQuery.of(context).size.width - 100,
+                      height: 230,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
                       ),
-                      Container(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 5, horizontal: 10),
-                          margin: const EdgeInsets.only(top: 20, right: 60),
-                          decoration: BoxDecoration(
-                              color: Colors.black.withOpacity(0.8),
-                              borderRadius: BorderRadius.circular(500)),
-                          child: Text(
-                            '${currentPageIndex + 1}/${feedData.image.length}',
-                            style: TextStyle(color: Colors.white),
-                          )),
-                    ]),
-                  ),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(15),
+                        child: PageView.builder(
+                            itemCount: feedData.image.length,
+                            onPageChanged: (value) {
+                              setState(() {
+                                currentPageIndex = value;
+                              });
+                            },
+                            itemBuilder: (BuildContext context, int index) {
+                              return Image.network(
+                                feedData.image.isNotEmpty &&
+                                        feedData.image.length > 0
+                                    ? feedData.image[index]
+                                    : 'https://firebasestorage.googleapis.com/v0/b/capstone2-1ad1d.appspot.com/o/smile%20pepe.png?alt=media&token=2240d1b9-b8cd-472a-81b8-d4f3be82335b',
+                                fit: BoxFit.cover,
+                              );
+                            }),
+                      ),
+                    ),
+                    Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                        margin: const EdgeInsets.only(top: 20, right: 60),
+                        decoration: BoxDecoration(
+                            color: Colors.black.withOpacity(0.8),
+                            borderRadius: BorderRadius.circular(500)),
+                        child: Text(
+                          '${currentPageIndex + 1}/${feedData.image.length}',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  ]),
                 ),
               ),
-            ]
-          ),
+            ),
+          ]),
           // 본문 컨테이너
           GestureDetector(
             onTap: () async {
-              var returnedValue = await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailPageWidget(
-                    feedData: feedData,
-                    isFavorite: isFavorite,
+              if (widget.feedData.reContentId == null) {
+                var returnedValue = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailPageWidget(
+                      feedData: feedData,
+                      isFavorite: isFavorite,
+                    ),
                   ),
-                ),
-              );
-              if (returnedValue != null && returnedValue is bool) {
-                setState(() {
-                  isFavorite = returnedValue;
-                });
+                );
+                if (returnedValue != null && returnedValue is bool) {
+                  setState(() {
+                    isFavorite = returnedValue;
+                  });
+                }
+              } else {
+                var returnedValue = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => RecontentsDetailPageWidget(
+                            feedData: feedData,
+                            isFavorite: isFavorite,
+                          )),
+                );
+                if (returnedValue != null && returnedValue is bool) {
+                  setState(() {
+                    isFavorite = returnedValue;
+                  });
+                }
               }
             },
             child: Container(
