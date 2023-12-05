b26a5602a8cc3f1e5ab95f385ca3731afc9ca6be
김재형 <Hiruca3220@gmail.com>
2023-12-06 00:18:53 +0900

    Revert "Revert "태그 페이지 현재 모든 피드를 태그 페이지에 표시되도록 구현 파라미터로 어떤 리스트를 넘기느냐에 따라 보여지는 피드가 달라질거임 / 내일 라이크, 리컨텐츠 기능 연결 그리고 태그 선택 리모컨 위젯 구현 기능 구현 마치고 태그 선택 가능하도록 구현으로 마무리""
    This reverts commit 2d1243ba0bd9b7d5caa9ef878e46b6e1e01d03ff.

--- lib/Home/TagPage/c_TagPageController.dart
+++ lib/Home/TagPage/c_TagPageController.dart
@@ -1,4 +1,9 @@
+import 'package:capstone/DataVO/model.dart';
 import 'package:capstone/Home/DetailPage/v_TagDetailPageWidget.dart';
 import 'package:get/get.dart';
 
-class TagController extends TagDetailPageWidget {}
+class TagController extends TagDetailPageWidget {
+  List<FeedDataVO> TagFeedList = DataVO.feedData;
+
+  TagController({required super.feedData});
+}
